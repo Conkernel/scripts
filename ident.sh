97aa85c8c2d70223bed9f35cae293965a3bf1454
ConKernel <conkernel@outlook.com>
2022-11-27 17:48:03 +0100

    retocado fichero iden.sh

--- ident.sh
+++ ident.sh
@@ -7,6 +7,9 @@ yum --version > /dev/null 2>&1
 retVal=$?
 if [ $retVal -eq 0 ]; then
 export distro="yum"
+# Descargamos y ejecutamos script para equipos yum.
+# ...
+return;
 Descargarse y ejecutar el fichero de usuario de yum
 fi
 # Apt
@@ -14,14 +17,18 @@ apt --version > /dev/null 2>&1
 retVal=$?
 if [ $retVal -eq 0 ]; then
 export distro="apt"
-Descargarse y ejecutar el fichero de usuario de apt
+# Descargamos y ejecutamos script para equipos apt.
+# ...
+return;
 fi
 #Arch
 pacman --version > /dev/null 2>&1
 retVal=$?
 if [ $retVal -eq 0 ]; then
 export distro="pacman"
-Descargarse y ejecutar el fichero de usuario de pacman
+# Descargamos y ejecutamos script para equipos pacman.
+# ...
+return;
 fi
 
 echo "No se ha podido identificar el tipo de distro"
