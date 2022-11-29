421083f920d0dacb48e747d47f1f29b4dfc682de
ConKernel <conkernel@outlook.com>
2022-11-29 17:33:23 +0100

    corrijo directorio de descarga de script packages

--- main.sh
+++ main.sh
@@ -14,7 +14,7 @@ chmod +x packages.sh
 echo
 echo "ESCRIBE LA CONTRASEÑA DE ROOT"
 echo
-su -c packages.sh
+su -c /tmp/custom_script/packages.sh
 # Descargamos y ejecutamos script para usuarios en yum.
 wget https://raw.githubusercontent.com/Conkernel/scripts/master/distros/yum/user.sh 
 chmod +x user.sh
